d9a856f68f58b63114f2b8301b5c43ebe9e461b4
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-08-02 01:16:20 -0300

    Inserindo dados no banco via API

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
 
         <nav className={styles.navbar}>
             <Container>
-                <a><Link to="/"><img className={styles.logo} src={logo} alt="Costs" /></Link></a>
+                <Link to="/"><img className={styles.logo} src={logo} alt="Costs" /></Link>
                 <ul className={styles.list}>
                     <li className={styles.item}><Link to="/">Home</Link></li>
                     <li className={styles.item}><Link to="/projects">Projetos</Link></li>
