7613c0e45855273a10f961db0d665b236f64d849
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-08-07 23:37:56 -0300

    Criando componente de loader

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,10 +5,13 @@ import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
 import { useState, useEffect } from 'react'
+import Loading from '../layout/Loading'
 
 function Projects() {
 
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+
     const location = useLocation()
     let message = ''
 
@@ -17,17 +20,20 @@ function Projects() {
     }
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json',
-            },
-        }).then(resp => resp.json())
-            .then(data => {
-                console.log(data)
-                setProjects(data)
-            })
-            .catch((err) => console.log(err))
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+            }).then(resp => resp.json())
+                .then(data => {
+                    console.log(data)
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))
+        }, 300)
     }, [])
 
     return (
@@ -47,6 +53,10 @@ function Projects() {
                         category={project.category.name}
                         key={project.id}
                     />)}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
